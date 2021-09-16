300ad513ab113172e91e8da5686bef74f1394546
Jeong Wan Seo <89565763+bachseo@users.noreply.github.com>
2021-09-16 15:20:31 +0900

    add second

--- index.php
+++ index.php
@@ -1,58 +1,18 @@
 <?php
-  function print_title() {
-    if(isset($_GET['id'])) {
-   echo $_GET['id'];
- } else {
-   echo "Welcome";
- }
-  }
-
-  function print_description() {
-    if(isset($_GET['id'])) {
-      echo file_get_contents("data/".$_GET['id']);
-    } else {
-      echo "Hello, PHP";
-    }
-  }
-
-  function print_list() {
-    $list=scandir('./data');
-    $i=0;
-    while ($i < count($list)) {
-      if($list[$i] != '.'){
-        if($list[$i] != '..'){
-      echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-      }
-    }
-      $i=$i+1;
-    }
-  }
- ?>
+  require_once('lib/print.php');
+  require('view/top.php');
+?>
 
 
-<!DOCTYPE html>
-<html lang="ko" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>
-        function print_title();
-    </title>
-  </head>
-  <body>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-      <?php
-       print_list();
+    <a href="create.php">create</a>
+    <?php if(isset($_GET['id'])) {?>
+      <a href="update.php?id=<?php echo $_GET['id'] ; ?>">update</a>
 
-      /*
-      echo "<li>$list[0]</li>\n";
-      echo "<li>$list[1]</li>\n";
-      echo "<li>$list[2]</li>\n";
-      echo "<li>$list[3]</li>\n";
-      echo "<li>$list[4]</li>\n";
-      */
-      ?>
-    </ol>
+      <form  action="delete_process.php" method="post">
+        <input type="hidden" name="id" value="<?=$_GET['id']?>"/>
+        <input type="submit" value="delete" />
+      </form>
+    <?php }  ?>
     <h2>
       <?php
         print_title();
@@ -62,5 +22,6 @@
       print_description();
      ?>
 
-  </body>
-</html>
+  <?php
+require('view/bottom.php');
+   ?>
